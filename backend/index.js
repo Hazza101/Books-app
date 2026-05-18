74a980244be69f8ba1b7f6eca5084e6d9bed352a
Harri <harri.bain@gmail.com>
2026-05-18 23:43:03 +0100

    Add dotenv file for mongo uri and port
    Connect to mongo database

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,26 @@
+require("dotenv").config();
 const express = require("express");
-const PORT = 3000;
-app = express();
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+const app = express();
 
 app.get("/", (response, request) => {
   request.send("<h1>Book app</h1>");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+async function main() {
+  try {
+    await mongoose.connect("hello");
+    console.log("Connected to the database.");
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+main();
